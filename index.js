1e99af6edecd1d861d6d69488b54cf0a802038ea
frongfrank17 <50213997+frongfrank17@users.noreply.github.com>
2020-04-14 01:01:30 +0700

    aaa

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/emergency/v1' , EmergencycellComntroller)
 const StationController = require('./Controller/StationService.Controller')
 app.use('/api/station/manager/v1' ,StationController)
 require("./Emergency.socket")
-
+/*
 app.use((req, res, next) => {
  
      res.header('Access-Control-Allow-Origin', '*')
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
      res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
      return next()
   })
-
+*/
 app.use((req, res, next) => {
   
     const err = new Error('Not found');
